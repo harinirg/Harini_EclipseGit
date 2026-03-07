d47f0de29149a125a54d02d7d973f8b29dfb9b52
Harini R G <harinirg26@gmail.com>
2026-03-07 20:40:38 +0530

    1st Commit updated

---
+++ EclipseGit_Harini/src/com/eclipseGit/PrintingStatement.java
@@ -0,0 +1,11 @@
+package com.eclipseGit;
+
+public class PrintingStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("EclipseGit");
+
+	}
+
+}
